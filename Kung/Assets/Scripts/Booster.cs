8203ffebb181b4c067c836b9eb34e0c9cac14386
cjs100412 <cjs100412@gmail.com>
2025-06-25 10:48:11 +0900

    fix : 부스터 기능 구현방식 수정

--- Kung/Assets/Scripts/Booster.cs
+++ Kung/Assets/Scripts/Booster.cs
@@ -6,15 +6,26 @@ public class Booster : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public PlayerMovement playerMovement;
     Animator[] animator;
+    private bool isBoost;
+    Rigidbody2D rb;
 
     private void Awake()
     {
-        //animator = playerMovement.GetComponentInChildren<Animator>();
         animator = playerMovement.GetComponentsInChildren<Animator>();
+        rb = playerMovement.GetComponent<Rigidbody2D>();
     }
-
+    private void Update()
+    {
+        if (isBoost)
+        {
+            //rb.linearVelocity = new Vector2(rb.linearVelocity.x, 1);
+            rb.AddForce(Vector2.up * 2, ForceMode2D.Force);
+        }
+    }
     public void OnPointerDown(PointerEventData eventData)
     {
+        isBoost = true;
+
         foreach (Animator anim in animator)
         {
             anim.SetBool("isBoost", true);
@@ -23,6 +34,8 @@ public class Booster : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        isBoost = false;
+
         foreach (Animator anim in animator)
         {
             anim.SetBool("isBoost", false);
