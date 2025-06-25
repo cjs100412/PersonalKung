367042b733ad0b3bc7336242065094e733d8e351
cjs100412 <cjs100412@gmail.com>
2025-06-25 11:20:50 +0900

    fix: 전체적인 뷰 깔끔하게 정리

--- Kung/Assets/Scripts/Booster.cs
+++ Kung/Assets/Scripts/Booster.cs
@@ -5,41 +5,30 @@ using UnityEngine.UIElements;
 public class Booster : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public PlayerMovement playerMovement;
-    Animator[] animator;
-    private bool isBoost;
+    public bool isBoost;
     Rigidbody2D rb;
 
     private void Awake()
     {
-        animator = playerMovement.GetComponentsInChildren<Animator>();
         rb = playerMovement.GetComponent<Rigidbody2D>();
     }
+
     private void Update()
     {
         if (isBoost)
         {
-            //rb.linearVelocity = new Vector2(rb.linearVelocity.x, 1);
-            rb.AddForce(Vector2.up * 2, ForceMode2D.Force);
+            rb.AddForce(new Vector2(0,0), ForceMode2D.Force);
         }
     }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         isBoost = true;
-
-        foreach (Animator anim in animator)
-        {
-            anim.SetBool("isBoost", true);
-        }
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         isBoost = false;
-
-        foreach (Animator anim in animator)
-        {
-            anim.SetBool("isBoost", false);
-        }
     }
 
 
