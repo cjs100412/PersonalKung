bd999e1bca1521233bda2397250e88ca24399397
KIDA1004K <kida1004k@gmail.com>
2025-07-03 18:05:18 +0900

    FIx : 충돌 해결

---
+++ Kung/Assets/Scripts/Inventory/UI/InventoryItemSlotUIData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public sealed class InventoryItemSlotUIData
+{
+    public int Quantity { get; }
+    public Sprite IconSprite { get; }
+
+    public InventoryItemSlotUIData(UserInventoryItemDto dto, IItemService itemService)
+    {
+        if (dto == null)
+        {
+            throw new ArgumentNullException(nameof(dto), "UserInventoryItemDto cannot be null");
+        }
+
+        Quantity = dto.Quantity;
+        IconSprite = Resources.Load<Sprite>(itemService.GetIconPath(dto.ItemId));
+
+        if (IconSprite == null)
+        {
+            throw new InvalidOperationException($"Icon sprite for Item ID {dto.ItemId} not found.");
+        }
+    }
+}
